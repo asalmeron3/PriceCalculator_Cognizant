fa4b8d4b68f64767dc95b037eb72e9be716bbaa1
Arturo Salmeron <asalmeron4@dhcp-10-101-246-34.wireless.northwestern.private>
2019-08-02 11:00:25 -0500

    Added tests for exceptionHandler & separated controller tests

--- src/test/java/com/cognizant/pricecalculator/controller/PriceControllerTest.java
+++ src/test/java/com/cognizant/pricecalculator/controller/PriceControllerTest.java
@@ -3,6 +3,8 @@ package com.cognizant.pricecalculator.controller;
 import com.cognizant.pricecalculator.model.PriceViewModel;
 import com.cognizant.pricecalculator.service.ServiceLayer;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import feign.FeignException;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
@@ -28,38 +30,72 @@ public class PriceControllerTest {
     private MockMvc mockMvc;
 
     private ObjectMapper om = new ObjectMapper();
+    private String pvmJson;
+    private String pvmExemptTaxJson;
 
-    @Test
-    public void testGetRoute() throws Exception {
+    @Before
+    public void setUp() throws Exception {
         PriceViewModel pvm = new PriceViewModel();
         pvm.setDescription("This is a test product. Tax exempt false");
 
         PriceViewModel pvmExemptTax = new PriceViewModel();
         pvmExemptTax.setDescription("This is a test product. Tax exempt true");
 
-        String pvmJson = om.writeValueAsString(pvm);
-        String pvmExemptTaxJson = om.writeValueAsString(pvmExemptTax);
-
+        pvmJson = om.writeValueAsString(pvm);
+        pvmExemptTaxJson = om.writeValueAsString(pvmExemptTax);
 
         Mockito.when(service.createPriceViewModel(1, 2, false)).thenReturn(pvm);
         Mockito.when(service.createPriceViewModel(1, 2, true)).thenReturn(pvmExemptTax);
+        Mockito.when(service.createPriceViewModel(0, 2, true)).thenThrow(FeignException.class);
 
+    }
+
+    @Test
+    public void testGetRouteQueryIncludesBothQuantityAndExemptTaxFalse() throws Exception {
 
         mockMvc.perform(MockMvcRequestBuilders.get("/api/price/product/1?quantity=2&exemptTax=false"))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().json(pvmJson));
+    }
 
+    @Test
+    public void testGetRouteWithQuantityPresentButExemptTaxIsNull() throws  Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/api/price/product/1?quantity=2"))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().json(pvmJson));
 
+    }
+
+    @Test
+    public void testGetRouteQueryIncludesBothQuantityAndExemptTaxTrue() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/api/price/product/1?quantity=2&exemptTax=true"))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().json(pvmExemptTaxJson));
-
-
     }
+
+
+    @Test
+    public void testGetRouteQuantityTypoIncludesStringShouldThrowNumberFormatException() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/price/product/1?quantity=2e&exemptTax=true"))
+                .andDo(print())
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void testGetRouteExemptTacBooleanTypoIncludesStringShouldThrowIllegalArgumentException() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/price/product/1?quantity=2&exemptTax=true9"))
+                .andDo(print())
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void testGetRouteProductIdNotFoundThrowsFeignException() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/price/product/0?quantity=2&exemptTax=true"))
+                .andDo(print())
+                .andExpect(status().isNotFound());
+    }
+
 }
